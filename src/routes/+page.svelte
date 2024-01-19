eb14a538995699f8a463cd3fd9d23a295a19436a
Diandra <diandrarizqy8@gmail.com>
2024-01-19 21:03:48 +0000

    implement simple long polling wait for data insertion to db

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,84 @@
 <script>
-  const api_url = '/data';
+  import {onMount} from 'svelte';
+    import Chart from 'chart.js/auto';
+    
+    let data = [20, 10, 30, 40];
+    let labels = ['a', 'b', 'c', 'd'];
+    let x = 0;
 
+    const api_url = '/data';
     const GetData = async () => {
-        const response = await fetch(`${api_url}`, {
+        const response = await fetch(`${api_url}?` + new URLSearchParams({tracker : x}), {
         method : 'GET',
           });
-        const data = await response.json();
-        console.log(data);
-    }
-  
+        const datas = await response.json();
+        console.log(datas);
+        let vals = datas.payload.map((d) => d.val);
+        let ts = datas.payload.map((d) => d.id);
+
+        data = vals;
+        labels = ts;
+        x = datas.tracker;
+
+        await GetData();
+      }
+
+    const InsertData = async () => {
+        const response = await fetch(`${api_url}`, {
+            method: 'POST',
+            headers: {
+                'Accept' : 'application/json',
+                'Content-Type' : 'application/json',
+              },
+            body: JSON.stringify({val : 15}),
+          });
+        const status = await response.json();
+        console.log(status);
+      }
+    
+    let ctx;
+    let canvas;
+    let chart;
+
+    let f = 2;
+
+    $: if (chart) {
+        console.log("x is changed");
+        chart.data.datasets[0].data = data;
+        chart.data.labels = labels;
+        chart.update();
+      }
+
+    onMount(async () => {
+        chart = new Chart(ctx, {
+            type: 'line',
+            data : {
+                labels : labels,
+                datasets: [
+                      {            
+                        label : 'ok',
+                        data : data,
+                      },
+                  ]
+              },
+          });
+      });
+
 </script>
 
-<h1>Welcome to SvelteKitosss</h1>
-<p>{api_url}</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Smart Coffee Roaster Dashboard</h1>
 <button on:click={GetData}>get</button>
+<button on:click={InsertData}>insert</button>
+<h2>tracker {x}</h2>
+<div width=400 height=200>
+  <canvas id="mychart" bind:this={ctx} width={200} height={50}></canvas>
+</div>
+
+<style>
+  /*
+  canvas {
+    width : 400;
+    height: 400;
+    background-color: red;
+  }*/
+</style>
--- src/routes/data/+server.js
+++ src/routes/data/+server.js
@@ -1,9 +1,33 @@
 import { error } from '@sveltejs/kit';
 import {sequelize, EnoseData} from '$lib/model/postgres.jsx';
 
+let tracker = 0;
+
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ url }) {	
+  const clientTracker = url.searchParams.get('tracker');
+  
+  while(clientTracker == tracker)
+    await new Promise(r => setTimeout(r, 100));
+  
   const enoseDatas = await EnoseData.findAll();
 
-  return new Response(JSON.stringify({payload: enoseDatas}));
+  return new Response(JSON.stringify({payload: enoseDatas, tracker : tracker}));
+}
+
+function flipTracker(){
+  if(tracker == 0) tracker = 1;
+  else tracker = 0;
+}
+
+export async function POST({ request }) {	
+  const {val} = await request.json();
+  
+  await EnoseData.create({
+    val: val,
+  });
+
+  flipTracker();
+
+  return new Response(JSON.stringify({status : 200}));
 }
