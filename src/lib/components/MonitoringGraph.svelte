05fe0b31dfd59323e0adb2b308cd6fb22ac30579
theunra <diandrarizqy7@gmail.com>
2024-02-03 13:37:30 +0700

    add history page

--- src/lib/components/MonitoringGraph.svelte
+++ src/lib/components/MonitoringGraph.svelte
@@ -22,8 +22,6 @@
         const darkIdx = enoseGraphData.roastLampIdx.dark;
         
         let graphDatas = enoseGraphData.sensorData.serialize(); 
-        // if(graphDataType == 'raw') graphDatas = rawData.serialize();
-        // else graphDatas = ppmData.serialize();
 
         const labels = enoseGraphData.labelData;
         
@@ -87,19 +85,6 @@
         }
 
         let chartDatasets = enoseGraphData.labels;
-        // if(graphDataType == 'raw') chartDatasets = [
-        //     'adc_mq135', 'adc_mq136', 'adc_mq137', 'adc_mq138', 
-        //     'adc_mq2', 'adc_mq3', 'adc_tgs822', 'adc_tgs2620'
-        // ];
-        // else chartDatasets = [
-        //     "mq135_co",          "mq135_alcohol",       "mq135_co2",        "mq135_toluen",     "mq135_nh4",        "mq135_aceton", 
-        //     "mq136_co",          "mq136_nh4",           "mq136_h2s",        "mq137_co",         "mq137_ethanol",    "mq137_nh3", 
-        //     "mq138_benzene",     "mq138_hexane",        "mq138_co",         "mq138_alcohol",    "mq138_propane",    "mq2_h2", 
-        //     "mq2_lpg",           "mq2_co",              "mq2_alcohol",      "mq2_propane",      "mq3_lpg",          "mq3_ch4", 
-        //     "mq3_co",            "mq3_alcohol",         "mq3_benzene",      "mq3_hexane",       "tgs822_methane",   "tgs822_co", 
-        //     "tgs822_isobutane",  "tgs822_hexane",       "tgs822_benzene",   "tgs822_ethanol",   "tgs822_acetone",   "tgs2620_methane", 
-        //     "tgs2620_co",        "tgs2620_isobutane",   "tgs2620_h2",       "tgs2620_ethanol"
-        // ];
 
         const chartDatasetsOption = chartDatasets.map((label) => ({            
             label : label,
@@ -112,11 +97,13 @@
         chart = new Chart(ctx, rawChartOptions);
     }
 
-      let height = 200;
-      let expand_text = "Expand";
+    let height = 200;
+    let expand_text = "Expand";
+
     onMount(async () => {
         CreateSensorGraphs();
     });
+
 </script>
 <div class="p" id="pp">
   <canvas id={graphId} class="graph" height={height} width=700 bind:this={ctx}></canvas>
